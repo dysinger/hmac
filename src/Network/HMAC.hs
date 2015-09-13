d6c4ddedfbf041d5d7969daaaa085fcbd3b566b2
Tim Dysinger <tim@fpcomplete.com>
2015-09-13 19:21:33 +0000

    wip checkpoint

--- src/Network/HMAC.hs
+++ src/Network/HMAC.hs
@@ -1,1 +1,63 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Network.HMAC where
+
+import           Control.Applicative ((<|>))
+import           Data.Attoparsec.ByteString.Char8
+import qualified Data.Attoparsec.ByteString.Char8 as A
+import           Data.ByteString
+import           Data.Word
+
+data ID =
+    ID ByteString
+    deriving (Eq,Show)
+
+data TS =
+    TS Integer
+    deriving (Eq,Show)
+
+data Nonce =
+    Nonce ByteString
+    deriving (Eq,Show)
+
+data Ext =
+    Ext ByteString
+    deriving (Eq,Show)
+
+data Mac =
+    Mac ByteString
+    deriving (Eq,Show)
+
+data Authorization = Authorization
+    { id' :: ID
+    , ts :: TS
+    -- , nonce :: Nonce
+    -- , ext :: Maybe Ext
+    -- , mac :: Mac
+    } deriving (Eq,Show)
+
+plainText = inClass "a-zA-Z0-9"
+
+padded x = skipMany space *> x <* skipMany space
+
+idParser :: Parser ID
+idParser = ID <$> padded (string "id=" *> takeWhile1 plainText)
+
+tsParser :: Parser TS
+tsParser = TS <$> padded (string "ts=" *> (toInteger <$> decimal))
+
+nonceParser :: Parser Nonce
+nonceParser = Nonce <$> padded (string "nonce=" *> takeWhile1 plainText)
+
+extParser :: Parser Ext
+extParser = Ext <$> padded (string "ext=" *> takeWhile1 plainText)
+
+macParser :: Parser Mac
+macParser = Mac <$> padded (string "mac=" *> takeWhile1 plainText)
+
+authParser :: Parser Authorization
+authParser = go idParser [tsParser, nonceParser, extParser, macParser]
+  go x ys =
+  -- try idParser ()
+  Authorization <$> idParser <*> tsParser -- <*> nonceParser <*> maybe extParser <*> macParser
